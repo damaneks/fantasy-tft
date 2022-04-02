9289432cbbdc4534339e6fdb58f3b1b6346c3354
Damian Wróblewski <damaneks@interia.pl>
2022-04-02 15:17:53 +0200

    feat(database): :card_file_box: add tournament model

--- fantasy/admin.py
+++ fantasy/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from .models import Player
+from .models import Player, Tournament
 
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'country']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
--- fantasy/models.py
+++ fantasy/models.py
@@ -11,3 +11,11 @@ class Player(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
