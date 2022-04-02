7fecdb886e20dcb420562526c9d7ac49b2fe8b2f
Damian Wróblewski <damaneks@interia.pl>
2022-04-02 15:38:23 +0200

    feat(database): :white_check_mark: add tournament model entry test

--- fantasy/tests/test_models.py
+++ fantasy/tests/test_models.py
@@ -15,3 +15,18 @@ class TestPlayerModel(TestCase):
         data = self.data1
         self.assertTrue(isinstance(data, Player))
         self.assertEqual(str(data), 'shircane')
+
+
+class TestTournamentModel(TestCase):
+
+    def setUp(self):
+        self.data1 = Tournament.objects.create(
+            name='Ultraliga Season 3', slug='ultraliga3')
+
+    def test_tournament_model_entry(self):
+        """
+        Test Tournament model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Tournament))
+        self.assertEqual(str(data), 'Ultraliga Season 3')
