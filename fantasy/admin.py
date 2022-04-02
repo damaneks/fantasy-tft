86be9052a1f8939760d14b2436d0df05aa7ca963
Damian Wróblewski <damaneks@interia.pl>
2022-04-02 15:06:54 +0200

    feat(database): :card_file_box: add player model

--- fantasy/admin.py
+++ fantasy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Player
+
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'country']
+    prepopulated_fields = {'slug': ('name',)}
--- fantasy/models.py
+++ fantasy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django_countries.fields import CountryField
 
-# Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    country = CountryField(blank=True)
+    twitter_url = models.URLField(blank=True)
+    twitch_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
